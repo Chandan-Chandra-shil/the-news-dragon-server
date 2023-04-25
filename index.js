2da829e1b9da72cb4cbced9fa0e930a914175a26
Chandan-Chandra-shil <chandanchandrashil22@gmail.com>
2023-04-25 16:49:47 +0600

    dynamic dada load using id

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PROT || 5000
 const cors = require('cors')
 const categories = require('./data/categories.json')
-
+const news = require ('./data/news.json')
 app.use(cors());
 
 
@@ -14,6 +14,35 @@ app.get('/', (req, res) => {
 app.get('/categories', (req, res) => {
   res.send(categories)
 })
+
+
+
+app.get('/news', (req, res) => {
+  res.send(news)
+})
+
+// i can't understand down 23/27 line
+app.get('/news/:id', (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find(n => n._id === id);
+  res.send(selectedNews)
+})
+
+app.get('/categories/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  
+  if (id === 0) {
+    res.send(news)
+  }
+  else {
+     const categoryNews = news.filter((n) => parseInt(n.category_id) === id);
+     res.send(categoryNews);
+  }
+ 
+})
+
+
+
 app.listen(port, () => {
   console.log(`Dragon API is running on port : ${port}`)
 })
